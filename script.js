04145fcfbecbed1e1ce3e5b160d3990fec496af0
Tayog <basallo.tayog@gmail.com>
2025-05-14 06:21:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleForms() {
+	const login = document.getElementById("loginForm");
+	const register = document.getElementById("registerForm");
+	login.classList.toggle("hidden");
+	register.classList.toggle("hidden");
+}
+
+
