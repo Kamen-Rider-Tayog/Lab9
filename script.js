09ad53a07a59fdc33acbd6e52fe936b9cc421e12
Tayog <basallo.tayog@gmail.com>
2025-05-15 03:18:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ document.getElementById("loginBtn").addEventListener("click", function (e) {
 	e.preventDefault();
 	const username = document.querySelector(".loginName").value.trim();
 	const password = document.querySelector(".loginName").value.trim();
+	const storedUsername = localStorage.getItem("registeredUsername");
+	const storedPassword = localStorage.getItem("registeredPassword");
 
 	if (username === "" || password === "") {
-		alert("Please enter both username and password");
+		alert("Fill up muna ngani.");
 		return;
+	}
+
+	if (username != storedUsername && password != storedPassword) {
+		alert("Bano mo naman, nakalimutan mo account mo.");
+	} else {
+		window.location.href = "website.html";
 	}
 });
 
@@ -21,39 +29,41 @@ document
 	.addEventListener("click", function (event) {
 		event.preventDefault();
 
-		
 		const username = document.querySelector(".registerName").value.trim();
 		const password = document.querySelector(".registerPass").value.trim();
-		const confirmPassword = document.querySelector(".registerConfirm").value.trim();
+		const confirmPassword = document
+			.querySelector(".registerConfirm")
+			.value.trim();
 		const errorElement = document.getElementById("passwordError");
 
 		errorElement.textContent = "";
+		const passwordPattern = /^[A-Z]/;
 
-		if (!username || !password || !confirmPassword) {
+		if (username === "" || password === "" || confirmPassword === "") {
 			alert("Mag fill up ka muna.");
 			return;
 		}
 
 		if (password !== confirmPassword) {
-			errorElement.classList.toggle("active");
-			errorElement.textContent = "Passwords do not match.";
+			alert("Hindi magkatulad, ayusin mo pag input.");
 			return;
 		}
 
 		if (password.length < 8) {
 			errorElement.classList.toggle("active");
-			errorElement.textContent = "Password must be at least 8 characters.";
+			errorElement.textContent = "Napaka ikli naman ng password mo.";
 			return;
 		}
 
-		if (password[0] !== password[0].toUpperCase()) {
+		if (!passwordPattern.test(password)) {
 			errorElement.classList.toggle("active");
-			errorElement.textContent =
-				"Password must start with an uppercase letter.";
+			errorElement.textContent = "MALAKING TITIK NGA MUNA.";
 			return;
 		}
+
+		localStorage.setItem("registeredUsername", username);
+		localStorage.setItem("registeredPassword", password);
 
 		alert("Registration successful!");
-		this.reset(); 
-		toggleForms(); 
+		toggleForms();
 	});
